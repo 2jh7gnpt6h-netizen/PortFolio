4098ce36595673542f604d6a7990dc6930847cf1
2jh7gnpt6h-netizen <2jh7gnpt6h@privaterelay.appleid.com>
2026-09-16 18:59:59 +0200

    Update data.js

--- assets/js/data.js
+++ assets/js/data.js
@@ -59,9 +59,9 @@ const CARNETS = [
     year: 2026,
     tags: ["Nature", "Volcans", "Temples"],
     countryCode: "id", // code ISO utilisé par la carte (assets/img/world-map.svg)
-    hero: "images/indonesie/DSCF5241.jpg",
+    hero: "images/indonesie/DSCF3072.jpg",
     heroAlt: "",
-    thumb: "images/indonesie/DSCF4289.jpg",
+    thumb: "images/indonesie/DSCF3072.jpg",
     photos: [
       { file: "images/indonesie/DSCF3019.jpg", alt: "", cap: "", meta: {} },
       { file: "images/indonesie/DSCF3072.jpeg", alt: "", cap: "", meta: {} },
